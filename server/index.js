aadc5959b7772c45a7b1cd834154ec2583c1a21a
pavanesh6666 <your-email@example.com>
2025-03-26 12:43:32 +0530

    commit changes

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const taskRoutes = require("./routes/taskRoutes");
+const taskRoutes = require("./routes/taskroutes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
